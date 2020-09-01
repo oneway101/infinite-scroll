6a7aa731206dde70391a315c70a035ad1b07e0f8
Ha Na Gill <oneway.first@gmail.com>
2020-09-01 18:07:15 -0400

    Initial batch loaded on the screen

--- src/index.js
+++ src/index.js
@@ -1,10 +1,36 @@
 import "./styles.css";
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+const container = document.querySelector(".container");
+
+function getDocumentHeight() {
+  const body = document.body
+  console.log(body.scrollHeight, body.clientHeight, body.offsetHeight)
+  return Math.max(body.scrollHeight, body.clientHeight, body.offsetHeight)
+}
+
+function getImageSrcUrl() {
+  const randomId = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER);
+  return "http://api.adorable.io/avatars/" + randomId;
+}
+
+function getElement() {
+  const element = document.createElement("div");
+  element.classList.add("card");
+  const image = new Image();
+  image.src = getImageSrcUrl();
+  element.appendChild(image);
+  return element;
+}
+
+function loadNextBatch(size = 10) {
+  for (let i = 0; i < size; i++) {
+    let element = getElement();
+    container.appendChild(element);
+  }
+}
+
+
+// laod the first batch
+loadNextBatch(20);
+
+getDocumentHeight()
