a7144160ca1f45724daf2035c6105cce65072465
Ha Na Gill <oneway.first@gmail.com>
2020-09-01 21:27:26 -0400

    Loads the next batch at the trigger position

--- src/index.js
+++ src/index.js
@@ -1,11 +1,24 @@
 import "./styles.css";
 
+// webkit (chrome) uses body for tracking scroll position
+const body = document.body
+// firefox and IE useus html for tracking the scroll position
+const html = document.documentElement
+
 const container = document.querySelector(".container");
 
 function getDocumentHeight() {
-  const body = document.body
-  console.log(body.scrollHeight, body.clientHeight, body.offsetHeight)
-  return Math.max(body.scrollHeight, body.clientHeight, body.offsetHeight)
+  const height = Math.max(
+    body.scrollHeight,
+    body.clientHeight,
+    html.scrollHeight,
+    html.clientHeight
+  )
+  return height
+}
+
+function getScrollLocation() {
+  return body.scrollTop || html.scrollTop
 }
 
 function getImageSrcUrl() {
@@ -22,15 +35,21 @@ function getElement() {
   return element;
 }
 
-function loadNextBatch(size = 10) {
+function loadNextBatch(size = 20) {
   for (let i = 0; i < size; i++) {
     let element = getElement();
     container.appendChild(element);
   }
 }
 
-
 // laod the first batch
-loadNextBatch(20);
+loadNextBatch();
 
-getDocumentHeight()
+// load more batches when user reached more than 90% of the bottom of the page
+window.onscroll = function(){
+  // 90 percentage trigger location
+  const triggerLocation = Math.round((getDocumentHeight() - window.innerHeight) * 0.9)
+  if (getScrollLocation() >= triggerLocation ) {
+    loadNextBatch()
+  }
+}
